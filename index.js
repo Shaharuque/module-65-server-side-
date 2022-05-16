6c25d7fbbe564cd5c0aa40fe3d55b84a769e3588
unknown <shahrukhamin27@gmail.com>
2022-05-17 01:07:06 +0600

    mongoDB connection done

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const app = express()
 const cors=require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const port = process.env.PORT || 5000;
 
 //use middlewares
 app.use(cors())
 //body parser ar kaj korbey(this is short cut)
 app.use(express.json())
+
+//mongoDB //password:0123
+const uri = "mongodb+srv://amin:0123@cluster0.yz2oh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const userCollection = client.db("food_express").collection("users");
+
+    // create a document to insert into database
+    const user = { 
+     name:'shik',
+     id:05,
+     email:'shaik@gmail.com'
+    }
+
+    const result = await userCollection.insertOne(user);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    // await client.close(); //dorkar nai connection active rakhtey chai
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
